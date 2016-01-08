b7cf45d289322df6309ed32639097d2bdd4f5181
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-08 12:13:26 -0500

    updated feature test file

--- spec/features/02_user_adds_car_spec.rb
+++ spec/features/02_user_adds_car_spec.rb
@@ -7,15 +7,15 @@ feature "user adds car manufacturer", %{
   So that I can list it in my lot
 
   Acceptance Criteria:
-  [] I must specify the manufacturer, color, year, and mileage of the car
+  [x] I must specify the manufacturer, color, year, and mileage of the car
   (an association between the car and an existing manufacturer should be created).
-  [] Only years from 1920 and above can be specified.
-  [] I can optionally specify a description of the car.
-  [] If I enter all of the required information in the required formats, the car
+  [x] Only years from 1920 and above can be specified.
+  [x] I can optionally specify a description of the car.
+  [x] If I enter all of the required information in the required formats, the car
   is recorded and I am presented with a notification of success.
-  [] If I do not specify all of the required information in the required formats,
+  [x] If I do not specify all of the required information in the required formats,
   the car is not recorded and I am presented with errors.
-  [] Upon successfully creating a car, I am redirected back to the index of cars.
+  [x] Upon successfully creating a car, I am redirected back to the index of cars.
 
 } do
 
